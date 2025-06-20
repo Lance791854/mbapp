c9f4441d882cc8a0c79c0ce2d042d8350949f11d
Lance791854 <lance.howell@my.jcu.edu.au>
2025-06-21 01:21:06 +1000

    Base Files

---
+++ app/src/main/java/com/example/unibookapp/AuthScreen.kt
@@ -0,0 +1,52 @@
+package com.example.unibookapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unibookapp.ui.theme.UniBookAppTheme
+import androidx.compose.ui.unit.dp
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UniBookAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    UniBookAppTheme {
+        LoginScreen()
+    }
+}
