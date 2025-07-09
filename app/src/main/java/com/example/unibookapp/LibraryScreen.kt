a6d282f60b4631d978947bb6d54e15f2c2cd3e47
g <g@users.noreply.github.com>
2025-07-09 19:17:27 +1000

    Improve library screen book item layout displaying items now within a card

--- app/src/main/java/com/example/unibookapp/LibraryScreen.kt
+++ app/src/main/java/com/example/unibookapp/LibraryScreen.kt
@@ -2,6 +2,7 @@ package com.example.unibookapp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,17 +15,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.unibookapp.ui.theme.UniBookAppTheme
 import com.example.unibookapp.data.Book
 import com.example.unibookapp.data.BookDao
 import com.example.unibookapp.data.UserBookDao
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.text.style.TextOverflow
 
 
 @Composable
@@ -56,15 +57,41 @@ fun LibraryScreen(
 
         Text(text = "My Books")
 
+
+
         LazyColumn {
             items(userBooks) { book ->
-                Column(
+
+                Card(
                     modifier = Modifier
                         .fillMaxWidth()
+                        .padding(4.dp)
                 ) {
-                    Text(
-                        text = book.title
-                    )
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Text(
+                                text = book.title,
+                                style = MaterialTheme.typography.titleMedium,
+                                maxLines = 2,
+                                overflow = TextOverflow.Ellipsis
+                            )
+
+                            Text(
+                                text = book.author,
+                                style = MaterialTheme.typography.bodySmall,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                        }
+                    }
                 }
             }
         }
