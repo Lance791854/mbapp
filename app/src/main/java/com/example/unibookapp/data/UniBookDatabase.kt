de3f9f10693e8e5e03ca66d8e704d1bf5efb64c0
g <g@users.noreply.github.com>
2025-07-14 17:38:44 +1000

    New database version

--- app/src/main/java/com/example/unibookapp/data/UniBookDatabase.kt
+++ app/src/main/java/com/example/unibookapp/data/UniBookDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class, Book::class, UserBook::class, Review::class], version = 2, exportSchema = false)
+@Database(entities = [User::class, Book::class, UserBook::class, Review::class], version = 3, exportSchema = false)
 abstract class UniBookDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun bookDao(): BookDao
