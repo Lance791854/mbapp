696b3e861c5d09937864df16a25d2016556e0192
g <g@users.noreply.github.com>
2025-07-14 16:42:48 +1000

    Add getReviewByUserAndBook query and update review

--- app/src/main/java/com/example/unibookapp/data/ReviewDao.kt
+++ app/src/main/java/com/example/unibookapp/data/ReviewDao.kt
@@ -2,11 +2,17 @@ package com.example.unibookapp.data
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface ReviewDao {
     @Insert
     suspend fun insert(review: Review): Long
 
-    // Need an update query
+    @Query("SELECT * FROM reviews WHERE bookId = :bookId AND username = :username LIMIT 1")
+    suspend fun getReviewByUserAndBook(bookId: String, username: String): Review?
+
+    @Update
+    suspend fun update(review: Review)
 }
