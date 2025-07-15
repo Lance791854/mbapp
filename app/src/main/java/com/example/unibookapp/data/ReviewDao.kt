d08ff3cfdd233c51de6bc60336a1d875102e833a
g <g@users.noreply.github.com>
2025-07-15 21:57:52 +1000

    Add OnConflictstrategy replace for inserting/updating reviews

--- app/src/main/java/com/example/unibookapp/data/ReviewDao.kt
+++ app/src/main/java/com/example/unibookapp/data/ReviewDao.kt
@@ -2,17 +2,15 @@ package com.example.unibookapp.data
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 
 @Dao
 interface ReviewDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(review: Review): Long
 
     @Query("SELECT * FROM reviews WHERE bookId = :bookId AND username = :username LIMIT 1")
     suspend fun getReviewByUserAndBook(bookId: String, username: String): Review?
-
-    @Update
-    suspend fun update(review: Review)
 }
