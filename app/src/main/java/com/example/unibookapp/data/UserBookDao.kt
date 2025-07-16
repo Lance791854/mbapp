759ae30f8d76888d351169b722f805280be74b14
g <g@users.noreply.github.com>
2025-07-16 17:10:46 +1000

    Add Query to update reading status

--- app/src/main/java/com/example/unibookapp/data/UserBookDao.kt
+++ app/src/main/java/com/example/unibookapp/data/UserBookDao.kt
@@ -14,6 +14,9 @@ interface UserBookDao {
     @Query("SELECT * FROM user_books WHERE username = :username")
     suspend fun getBooksByUser(username: String): List<UserBook>
 
+    @Query("UPDATE user_books SET readingStatus = :status WHERE username = :username AND bookId = :bookId")
+    suspend fun updateReadingStatus(username: String, bookId: String, status: String)
+
     @Delete
     suspend fun delete(userBook: UserBook)
 }
