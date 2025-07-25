2209eb337d0e1e86cfb6b7ef1ca2c6e70a5b1c14
g <g@users.noreply.github.com>
2025-07-26 03:11:06 +1000

    Start of profile page with simple display

--- app/src/main/java/com/example/unibookapp/ProfileScreen.kt
+++ app/src/main/java/com/example/unibookapp/ProfileScreen.kt
@@ -1,19 +1,36 @@
 package com.example.unibookapp
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.unibookapp.ui.theme.UniBookAppTheme
+import com.example.unibookapp.data.ReviewDao
+import com.example.unibookapp.data.UserBook
+import com.example.unibookapp.data.UserBookDao
 
 @Composable
 fun ProfileScreen(
+    username: String,
+    userBookDao: UserBookDao,
+    reviewDao: ReviewDao,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -21,17 +38,34 @@ fun ProfileScreen(
         horizontalAlignment  = Alignment.CenterHorizontally,
         modifier = modifier
             .fillMaxSize()
-            .padding(8.dp)
+            .padding(16.dp)
     ) {
-        Text(text = "ProfileScreen")
+
+        var userBooks by remember { mutableStateOf<List<UserBook>>(emptyList()) }
+
+        LaunchedEffect(username) {
+            userBooks = userBookDao.getBooksByUser(username)
+        }
+
+
+        Text(text = "Profile", style = MaterialTheme.typography.displaySmall)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            border = BorderStroke(1.dp, Color.Black)
+        ) {
+            Text(
+                text = username,
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Center
+            )
+        }
+
+
+
     }
 }
 
-
-@Preview(showBackground = true)
-@Composable
-fun ProfilePreview() {
-    UniBookAppTheme {
-        ProfileScreen()
-    }
-}
