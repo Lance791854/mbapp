6a9593b1ef5afe8ebdb6234a95a2596106eeee47
g <g@users.noreply.github.com>
2025-07-25 20:54:36 +1000

    Improve layout and added more toggles including hides reviews and ratings from public

--- app/src/main/java/com/example/unibookapp/SettingsScreen.kt
+++ app/src/main/java/com/example/unibookapp/SettingsScreen.kt
@@ -5,20 +5,25 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.unibookapp.ui.theme.UniBookAppTheme
 
 
 @Composable
@@ -28,41 +33,116 @@ fun SettingsScreen(
     onLogoutClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+    var isRatingsPublic by remember { mutableStateOf(false) }
+    var isReviewsPublic by remember { mutableStateOf(false) }
+
+
     Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment  = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
             .fillMaxSize()
             .padding(8.dp)
     ) {
-        Text(text = "SettingsScreen")
-        Text(
-            text = "Logout",
-            color = Color.Blue,
-            modifier = Modifier.clickable { onLogoutClick() }
-        )
+        Text(text = "Settings", style = MaterialTheme.typography.displaySmall)
 
 
-    }
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        border = BorderStroke(1.dp, Color.Black)
-    ) {
-        Row(
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            border = BorderStroke(1.dp, Color.Black)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Display ratings publicly",
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Switch(
+                    checked = isRatingsPublic,
+                    onCheckedChange = { isRatingsPublic = it }
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            border = BorderStroke(1.dp, Color.Black)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Display reviews publicly",
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Switch(
+                    checked = isReviewsPublic,
+                    onCheckedChange = { isReviewsPublic = it }
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+
+
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            border = BorderStroke(1.dp, Color.Black)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Dark Mode",
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Switch(
+                    checked = isDarkMode == true,
+                    onCheckedChange = { onThemeChange(it) }
+                )
+
+            }
+        }
+
+
+        Spacer(modifier = Modifier.weight(1f)) // Push logout to bottom of page
+
+
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .clickable { onLogoutClick() },
+            border = BorderStroke(1.dp, Color.Black)
         ) {
             Text(
-                text = "Dark Mode",
-                style = MaterialTheme.typography.titleMedium
-            )
-
-            Switch(
-                checked = isDarkMode == true,
-                onCheckedChange = { onThemeChange(it) }
+                text = "Logout",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
+                color = Color.Red
             )
         }
     }
